f1fb688d99d51d48bf7852bb595e02c5501cee85
KoenVergeer123 <D290735@edu.curio.nl>
2023-11-09 12:31:33 +0100

    nav-bar+homepage

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<x-app-layout>
+
+    <div class="bgimg">
+        <img src="{{asset('/image/Homepage.png')}}">
+        <div class="middle">
+            <h1>BarrocIntens</h1>
+            <hr>
+            <p>Coming online soon</p>
+        </div>
+        <div class="bottomleft">
+            <p></p>
+        </div>
+    </div>
+
+</x-app-layout>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,25 +1,25 @@
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-0">
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <img src="{{asset('/image/NavbarlogoSmall.png')}}" class="block h-12 w-auto fill-current text-gray-800" />
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('dashboard') }}
+                        {{ __('Home') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                        {{ __('Koffie Merken') }}
                     </x-nav-link>
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Machines') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Contact') }}
                     </x-nav-link>
                 </div>
             </div>
