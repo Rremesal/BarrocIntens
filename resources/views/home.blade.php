35371ddd8f1e793ae14b51ff6e3454237c9e1ab0
KoenVergeer123 <D290735@edu.curio.nl>
2023-11-13 11:11:25 +0100

    ?

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,13 @@
 <x-app-layout>
 
     <div class="bgimg">
-        <img src="{{asset('/image/Homepage.png')}}">
+        <div class="overlay">
+            <img src="{{ asset('/image/Homepage.png') }}" class="image">
+        </div>
         <div class="middle">
-            <h1 class="text-8xl	font-extrabold">KOFFIEBRAND BARROCINTENS</h1>
+            <h1 class="text-8xl font-extrabold">KOFFIEBRAND BARROCINTENS</h1>
             <hr>
-            <p class="text-base	">Informatie over het bedrijf...   Informatie over het bedrijf...   Informatie over het bedrijf...   Informatie over het bedrijf...   Informatie over het bedrijf...   Informatie over het bedrijf...   </p>
+            <p class="text-base">Informatie over het bedrijf... Informatie over het bedrijf... Informatie over het bedrijf... Informatie over het bedrijf... Informatie over het bedrijf... Informatie over het bedrijf...</p>
             <br>
             <button class="bg-black hover:bg-black text-white font-semibold py-2 px-4 border border-white rounded shadow">Koffie Merken</button>
             <button class="bg-black hover:bg-black text-white font-semibold py-2 px-4 border border-white rounded shadow">Koffie Machines</button>
