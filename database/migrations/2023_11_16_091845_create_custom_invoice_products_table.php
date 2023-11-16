0bf6cba7481a74e3c136722316a1ec9709e7c2b2
JensvanDijk2004 <d290681@edu.curio.nl>
2023-11-16 10:29:57 +0100

    created model, controller, migrations

---
+++ database/migrations/2023_11_16_091845_create_custom_invoice_products_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('custom_invoice_products', function (Blueprint $table) {
+            $table->id('custom_invoice_id');
+            $table->int('amount');
+            $table->decimal('price_per_product');
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('custom_invoice_products');
+    }
+};
