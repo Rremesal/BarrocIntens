4b162ade47f44ec8f480c8cccc32234b6e51a797
Raul Remesal van Merode <raul.remesal@gmail.com>
2023-11-27 17:54:43 +0100

    resource naam veranderd voor producten

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\dashboardController;
+use App\Http\Controllers\MachineController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -34,6 +35,7 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::resource('/dashboard', dashboardController::class);
+Route::resource('/product', MachineController::class);
 
 
 require __DIR__.'/auth.php';
