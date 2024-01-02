e666baa2d6da43d97baed322f2a1534f122d3e99
Raul Remesal van Merode <raul.remesal@gmail.com>
2024-01-02 09:45:46 +0100

    changed the product factory file and description fields

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Http\UploadedFile;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
@@ -18,8 +19,8 @@ class ProductFactory extends Factory
     {
         return [
             "name" => fake()->name(),
-            "description" => fake()->paragraph(),
-            "file" => fake()->imageUrl(),
+            "description" => fake()->text(140),
+            "file" => UploadedFile::fake()->image('thumbnail.jpg', 100, 100)->store('thumbnails', 'public'),
             "price" => fake()->randomFloat(2, 20, 30),
             "product_category_id" => fake()->numberBetween(1,2),
             "created_at" => now(),
