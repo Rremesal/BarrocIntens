d9505a82b0de80428a2d788c4619f8af873a782f
JensvanDijk2004 <d290681@edu.curio.nl>
2024-01-08 09:00:40 +0100

    Notification route weggehaald

--- routes/web.php
+++ routes/web.php
@@ -48,7 +48,7 @@ Route::middleware('auth')->group(function () {
 
     Route::resource('/product', ProductController::class);
     Route::put('stockchange/{item}', [StockchangeController::class, 'update']);
-    Route::resource('/notification', NotificationController::class);
+    // Route::resource('/notification', NotificationController::class);
 
 
     Route::resource('/product', ProductController::class);
