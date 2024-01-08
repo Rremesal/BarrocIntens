6b6626cec6a1b47766b221ecaedc5618ab972770
KoenVergeer123 <D290735@edu.curio.nl>
2024-01-08 08:39:43 +0100

    Fix Mergeconflict

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,21 +1,24 @@
 <x-app-layout>
     <x-cookie-alert></x-cookie-alert>
-    <div class="bgimg">
-        <div class="overlay">
-            <img src="{{ asset('/image/Homepage.png') }}" class="image">
-        </div>
-        <div class="middle">
-            <h1 class="text-8xl font-extrabold">KOFFIEBRAND BARROCINTENS</h1>
-            <hr>
-            <p class="text-base">Informatie over het bedrijf... Informatie over het bedrijf... Informatie over het bedrijf... Informatie over het bedrijf... Informatie over het bedrijf... Informatie over het bedrijf...</p>
-            <br>
-            <a href="{{ route('machines.index') }}" class="bg-black hover:bg-black text-white font-semibold py-2 px-4 border border-white rounded shadow">
-                Koffie Merken</a>
-            </button>
-            <a href="{{ route('machines.index') }}" class="bg-black hover:bg-black text-white font-semibold py-2 px-4 border border-white rounded shadow">
-                Koffie Machines
-            </a>
-            </button>
+
+        <div class="bgimg">
+            <div class="overlay">
+                <div class="bggradient">
+                <img src="{{ asset('/image/Homepage.png') }}" class="image">
+            </div>
+            <div class="middle">
+                <h1 class="text-8xl font-extrabold">KOFFIEBRAND BARROCINTENS</h1>
+                <hr>
+                <p class="text-base">Informatie over het bedrijf... Informatie over het bedrijf... Informatie over het bedrijf... Informatie over het bedrijf... Informatie over het bedrijf... Informatie over het bedrijf...</p>
+                <br>
+                <a href="{{ route('machines.index') }}" class="bg-black hover:bg-black text-white font-semibold py-2 px-4 border border-white rounded shadow">
+                    Koffie Merken</a>
+                </button>
+                <a href="{{ route('machines.index') }}" class="bg-black hover:bg-black text-white font-semibold py-2 px-4 border border-white rounded shadow">
+                    Koffie Machines
+                </a>
+                </button>
+            </div>
         </div>
     </div>
 </x-app-layout>
