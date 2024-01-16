4043ebd348826e2d43ccede132a48a6167df7704
Raul Remesal van Merode <raul.remesal@gmail.com>
2024-01-16 20:42:19 +0100

    Revert to 77152a6

--- resources/views/producten/index.blade.php
+++ resources/views/producten/index.blade.php
@@ -57,6 +57,49 @@
         </div>
     @else
         {{-- hier moet de view komen voor de klanten (dit staat op koen zijn to-do) --}}
+{{--    <div class="grid justify-center items-center grid-cols-3 mx-auto">--}}
+{{--        <div class=" w-80 h-96 flex flex-col rounded-lg hover:shadow-2xl border shadow-lg relative hover:cursor-pointer">--}}
+{{--            <div class="inset-3 flex items-center justify-center p-3">--}}
+{{--                <img class="w-100 h-100 object-cover" src="{{ asset('/image/machine1.png') }}" >--}}
+{{--            </div>--}}
+{{--            <div class="text-black h-40 mt-auto text-center rounded-b-lg font-extrabold" >--}}
+{{--                NESPRESSO <BR>--}}
+{{--                AGUILA 440--}}
+{{--                <br>--}}
+{{--                <br>--}}
+{{--                <br>--}}
+{{--                <br>--}}
+{{--                <button class="bg-yellow-300">Ontdek Machine</button>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+        <a href="#" class="styled-card w-80 h-104 flex flex-col rounded-lg overflow-hidden hover:shadow-2xl border shadow-lg relative hover:cursor-pointer">
+            <div class="image-container flex items-center justify-center">
+                <img class="w-full h-full object-cover rounded-t-lg" src="{{ asset('/image/machine1.png') }}" alt="Styled Card Image">
+            </div>
+            <div class="text-container text-white h-56 flex flex-col items-center justify-center rounded-b-lg px-4 bg-gradient-to-t from-gray-900 to-transparent">
+                <h2 class="text-3xl font-extrabold leading-tight">Elegant Machine</h2>
+                <div class="mt-2 line-container flex items-center">
+                    <div class="line h-2 w-16 border-t-2 border-black mr-10"></div>
+                    <div class="line h-2 w-16 border-t-2 border-black ml-10"></div>
+                </div>
+                <p class="mt-2 text-sm italic">Unleash the power of innovation.</p>
+                <button class="bg-black hover:bg-gray-800 text-white mt-3 py-2 px-4 rounded-full focus:outline-none">Discover</button>
+            </div>
+        </a>
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     @endif
 
